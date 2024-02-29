beb1a6558b61937cc706d5310db292ece84887ed
hajune <buea486@gmail.com>
2024-02-29 17:58:10 +0900

    paging dto

--- src/main/java/com/june/apiserver/dto/PageRequestDto.java
+++ src/main/java/com/june/apiserver/dto/PageRequestDto.java
@@ -1,16 +1,20 @@
 package com.june.apiserver.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+@Data
 @Builder
 @SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PageRequestDto {
     @Builder.Default
     private int page = 1;
 
     @Builder.Default
     private int size = 10;
-
-
 }
--- src/main/java/com/june/apiserver/dto/PageResponseDto.java
+++ src/main/java/com/june/apiserver/dto/PageResponseDto.java
@@ -1,5 +1,44 @@
 package com.june.apiserver.dto;
 
-public class PageResponseDto<E> {
+import lombok.Data;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Data
+public class PageResponseDto<E> {
+    // * 진짜 데이터, 목록 데이터
+    private List<E> dtoList;
+    private List<Integer> pageNumberList;
+    private PageRequestDto pageRequestDto;
+    private boolean prev, next;
+    private int totalCount, prevPage, nextPage, totalPage, current;
+
+    public PageResponseDto(List<E> dtoList, PageRequestDto pageRequestDto, long total) {
+        this.dtoList = dtoList;
+        this.pageRequestDto = pageRequestDto;
+        this.totalCount = (int) total;
+
+        // * 끝페이지 end
+        int end = (int) (Math.ceil(pageRequestDto.getPage() / 10.0)) * 10;
+
+        // * 시작 번호
+        int start = end - 9;
+
+        // * 진짜 마지막 값
+        int last = (int) Math.ceil(totalCount / (double) pageRequestDto.getSize());
+
+        // * 끝 번호
+        end = end > last ? last : end;
+
+        this.prev = start > 1;
+        this.next = totalCount > end * pageRequestDto.getSize();
+
+        // * 페이지 번호 리스트
+        this.pageNumberList = IntStream.rangeClosed(start, end).boxed().collect(Collectors.toList());
+
+        this.prevPage = prev ? start - 1 : 0;
+        this.nextPage = next ? end + 1 : 0;
+    }
 }
