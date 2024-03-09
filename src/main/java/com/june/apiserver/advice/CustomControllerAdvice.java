e3d315bed761964a1f179e857fb094247418efc4
hajune <buea486@gmail.com>
2024-03-09 20:48:41 +0900

    REST API Exception 처리

--- src/main/java/com/june/apiserver/advice/CustomControllerAdvice.java
+++ src/main/java/com/june/apiserver/advice/CustomControllerAdvice.java
@@ -1,2 +1,31 @@
-package com.june.apiserver.advice;public class CustomController {
+package com.june.apiserver.advice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+@Slf4j
+@RestControllerAdvice
+public class CustomControllerAdvice {
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<?> noExist(NoSuchElementException e) {
+        log.error("error = {}", e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("message", e.getMessage()));
+    }
+    //  NoResourceFoundException
+    //  MethodArgumentTypeMismatchException
+    //  MethodArgumentNotValidException
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> no(MethodArgumentNotValidException e) {
+        log.error("error = {}", e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(Map.of("message", e.getMessage()));
+    }
+
 }
