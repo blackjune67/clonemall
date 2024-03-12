4886ff71e67581c60a4f57130bc43e2d8391d315
hajune <buea486@gmail.com>
2024-03-12 21:56:20 +0900

    CORS 설정

--- src/main/java/com/june/apiserver/config/CustomServletConfig.java
+++ src/main/java/com/june/apiserver/config/CustomServletConfig.java
@@ -4,6 +4,7 @@ import com.june.apiserver.config.formatter.LocalDateFormatter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -13,5 +14,14 @@ public class CustomServletConfig implements WebMvcConfigurer {
     public void addFormatters(FormatterRegistry registry) {
         log.info("addFormatters..........");
         registry.addFormatter(new LocalDateFormatter());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .maxAge(500)
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS");
+//        WebMvcConfigurer.super.addCorsMappings(registry);
     }
 }
