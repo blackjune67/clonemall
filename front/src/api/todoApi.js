793adedbac5c49bc580bf36085a482a36aea37a8
hajune <buea486@gmail.com>
2024-03-20 11:59:40 +0900

    파라미터 수정

--- front/src/api/todoApi.js
+++ front/src/api/todoApi.js
@@ -4,7 +4,7 @@ export const API_SERVER_HOST = 'http://localhost:8080'
 
 const prefix = `${API_SERVER_HOST}/api/todo`
 
-export const getOne = async (id) => {
+export const getOne = async (tno) => {
   const response = await axios.get(`${prefix}/${tno}`)
   return response.data
 }
