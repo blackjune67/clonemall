cdbfa6f616f0bbcf2e22de4c8766409eaddaecdb
hajune <buea486@gmail.com>
2024-03-29 07:51:03 +0900

    게시글 한개 클릭

--- front/src/components/todo/ListComponent.js
+++ front/src/components/todo/ListComponent.js
@@ -18,7 +18,7 @@ const initState = {
 }
 
 function ListComponent(props) {
-  const {page, size, moveToList, refresh} = useCustomMove();
+  const {page, size, refresh , moveToList, moveToRead} = useCustomMove();
   const [serverData, setServerData] = useState(initState);
 
   useEffect(() => {
@@ -33,7 +33,9 @@ function ListComponent(props) {
         Todo List Component
         <div className="flex flex-wrap mx-auto justify-center p-6">
           {serverData.dtoList.map(todo =>
-              <div key={todo.tno} className="w-full min-w-[400px] p-2 m-2 rounded shadow-md">
+              <div key={todo.tno} className="w-full min-w-[400px] p-2 m-2 rounded shadow-md"
+                onClick={() => moveToRead(todo.tno)}
+              >
                 <div className="flex">
                   <div className="font-extrabold text-2xl p-2 w-1/12">
                     {todo.tno}
