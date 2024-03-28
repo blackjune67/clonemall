8897ba1015afd36b806ec4e0a22f11a63f2e66a7
hajune <buea486@gmail.com>
2024-03-29 07:30:31 +0900

    읽기 컴포넌트 생성

--- front/src/components/todo/ReadComponent.js
+++ front/src/components/todo/ReadComponent.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {getOne} from "../../api/todoApi";
+import useCustomMove from "../../hooks/useCustomMove";
 
 const initState = {
   tno: 0,
@@ -12,6 +13,8 @@ const initState = {
 
 function ReadComponent({tno}) {
   const [todo, setTodo] = useState(initState);
+
+  const {moveToList, moveToModify} = useCustomMove();
 
   useEffect(() => {
     getOne(tno).then(data => {
@@ -28,6 +31,23 @@ function ReadComponent({tno}) {
         {makeDiv('Content', todo.content)}
         {makeDiv('Due Date', todo.dueDate)}
         {makeDiv('Completed', todo.completed ? 'Completed' : 'Not Yet')}
+
+        <div className="flex justify-end p-4">
+          <button type="button"
+                  className={"round p-4 m-2 text-xl w-32 text-white bg-blue-500"}
+                  onClick={() => moveToList()}
+          >
+            List
+          </button>
+
+          <button type="button"
+                  className={"round p-4 m-2 text-xl w-32 text-white bg-red-500"}
+                  onClick={() => moveToModify(todo.tno)}
+          >
+            Modify
+          </button>
+        </div>
+
       </div>
   );
 }
@@ -36,7 +56,7 @@ const makeDiv = (title, value) => {
   return (
       <div className="flex justify-center">
         <div className="relative mb-4 flex w-full flex-wrap items-stretch">
-          <div className="w-1/5 p-6 text-right font-bold">{title}</div>
+        <div className="w-1/5 p-6 text-right font-bold">{title}</div>
           <div className="w-4/5 p-6 rounded-r border border-solid shadow-md">{value}</div>
         </div>
       </div>
