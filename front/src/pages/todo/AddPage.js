d15f6a2223d0b7eca7ad0fb52395718edfc94ac7
hajune <buea486@gmail.com>
2024-04-01 16:41:16 +0900

    front add component

--- front/src/pages/todo/AddPage.js
+++ front/src/pages/todo/AddPage.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import AddComponent from "../../components/todo/AddComponent";
 
 function AddPage(props) {
   return (
-      <div className={'text-3xl font-extrabold'}>
-        추가 페이지입니다만??
+      <div className={"p-4 w-full bg-white"}>
+        <div className={"text-3xl font-extrabold"}>
+          Todo Add Page
+        </div>
+        <AddComponent/>
       </div>
   );
 }
